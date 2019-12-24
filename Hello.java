f2d7cbb49fe41780d7a50815e2f381bcfa359bc5
Michael <Michael.Song@huameisoft.cn>
2019-12-24 12:45:45 +0800

    Last Commit

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 		System.out.println("Hello Git!");
 		System.out.println("Hello World!");
 		System.out.println("Hello Test！");
+		System.out.println(new java.util.Date());
 	}
 }
