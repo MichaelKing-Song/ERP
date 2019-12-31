73d738fe8a10b7d5e90533ba9abdd4cf331ba556
Michael <Michael.Song@huameisoft.cn>
2019-12-31 12:43:55 +0800

    Test Meger - Change Hello.java

--- Hello.java
+++ Hello.java
@@ -3,11 +3,6 @@ import java.util.*;
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello~");
-		System.out.println("Hello Git!");
-		System.out.println("Hello World!");
-		System.out.println("Hello Test！");
-		System.out.println(new java.util.Date());
-
 		Admin admin = new Admin();
 		Emp emp = new Emp();
 		System.out.println("Study hard, day day up!!!");
