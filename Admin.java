6311d54d468b57c879afc77c2f14c7ed1ba1dc36
Michael <Michael.Song@huameisoft.cn>
2020-01-03 21:51:19 +0800

    Change Admin.java --Author: gitpro

--- Admin.java
+++ Admin.java
@@ -1,6 +1,8 @@
+package cn.itmk.demo;
+
 import java.util.List;
 public class Admin {
 	private List<Emp> emps;
-	private String aid;
 	private List<Action> actions = new ArrayList<Action>();
+	private String name;
 }
